b6998d7cecbfc033612aca7ed65f7e4c9ebd5f49
Alexander Wright <xander.h.wright@gmail.com>
2022-08-04 14:55:41 -0400

    Made component to display project data

---
+++ src/components/ProjectContainer.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ProjectDisplay from './ProjectDisplay.vue'
+import projectData from '../data/projects.json'
+</script>
+<script>
+export default {
+  data() {
+    return {
+      colors: ["#8ACB88", "#CAE5FF", "#2E4057", "#ACEDFF", "#697A21"],
+      projects: projectData
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div v-for="project in projectData" :key="project.id">
+      <ProjectDisplay :project="project" :colors="colors"></ProjectDisplay>
+    </div>
+  </div>
+</template>
+
+<style>
+.container{
+  float:left;
+  background-color: v-bind(colors[4]);
+  width:100%;
+  position: absolute;
+  left: 0;
+}
+</style>
---
+++ src/components/ProjectDisplay.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: ["project", 'colors'],
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+<div>
+  <h2>{{project.title}}</h2>
+  <p>{{project.description}}</p>
+  <img :src="this.project.image" :alt="this.project.imageAlt">
+</div>
+
+</template>
+
+<style scoped>
+h2{
+  color:v-bind(colors[2]);
+}
+p{
+  color:v-bind(colors[2]);
+}
+div {
+  background-color: v-bind(colors[1]);
+  padding: 1%;
+  margin: 1%;
+  float: left;
+  width: 46%;
+}
+img {
+  height: 25%;
+  width: 25%;
+}
+@media screen and (max-width: 700px) {
+  div {
+    width: 100%;
+  }
+}
+</style>
